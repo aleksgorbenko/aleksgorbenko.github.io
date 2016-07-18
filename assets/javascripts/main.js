98b78000f5635ddb678f7a2eadc8480fc4c164b0
Aleks Gorbenko <kucher12@gmail.com>
2016-07-18 16:22:05 +0100

    added smooth scroll for on-page links, added delays for animations to balance smooth scroll

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -4,7 +4,11 @@ $(function() {
   // CTA animation on navbar link clicks
   $('a:contains("CONTACT")').click(function () {
     var $contact = $('#btn-contact');
-    $contact.addClass('animated tada');
+
+    setTimeout(function() {
+      $contact.addClass('animated tada');
+    }, 1000);
+
     setTimeout(function() {
       $contact.removeClass('animated tada');
     }, 3000);
@@ -12,7 +16,10 @@ $(function() {
 
   $('a:contains("RESUME")').click(function () {
     var $resume = $('#btn-resume');
-    $resume.addClass('animated flash');
+    setTimeout(function() {
+      $resume.addClass('animated flash');;
+    }, 1000);
+
     setTimeout(function() {
       $resume.removeClass('animated flash');
     }, 3000);
@@ -20,7 +27,7 @@ $(function() {
 
   // navbar transparency on scroll
   $(document).ready(function(){
-    var scroll_start = 1000;
+    var scroll_start = 0;
     var startchange = $('.navbar');
     var offset = startchange.offset();
     $(document).scroll(function() {
@@ -31,5 +38,19 @@ $(function() {
           $('.navbar').css('background-color', 'transparent');
        }
      });
+  });
+
+  // smooth scrolling
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
   });
 });
